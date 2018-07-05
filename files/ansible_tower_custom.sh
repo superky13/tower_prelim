1d398f0d3c94b71f9f57e3dbad1d8d829b491f59
root <root@ky13-desktop.ky13buildbox.com>
2018-07-05 12:20:18 -0400

    abstracted tower admin user and pw

--- files/ansible_tower_custom.sh
+++ files/ansible_tower_custom.sh
@@ -1,1 +1,1 @@
-ANSIBLE_BECOME_METHOD=sudo ANSIBLE_BECOME=true ansible-playbook -i inventory_custom -e bundle_install install.yml
+ANSIBLE_BECOME_METHOD=sudo ANSIBLE_BECOME=true ansible-playbook -i inventory_custom -e bundle_install=true install.yml
